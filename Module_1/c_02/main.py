e60f7ee7395f8f0da1225244544b0cb50df466c1
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-21 07:29:43 -0500

    module 1 - class 2

---
+++ Module_1/c_02/main.py
@@ -0,0 +1,22 @@
+# lower camel case
+def myApiFunction():
+    pass
+
+
+# upper camel case
+class MyClass:
+    pass
+
+
+# PEP8
+
+# snake case
+def my_api_function():
+    # screaming snake case
+    VERSION_API = 1
+
+    # protected
+    _protected = "Im protected"
+
+    # private
+    __private = "Im private"
