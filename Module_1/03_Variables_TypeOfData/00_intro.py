ec7b0f6135071f70009af3052fb954fb1eeeb7d9
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-21 07:48:38 -0500

    module 1 - class 3 _ intro

---
+++ Module_1/03_Variables_TypeOfData/00_intro.py
@@ -0,0 +1,13 @@
+test_ip_address = "193.192.1.45"
+print(test_ip_address)
+print(type(test_ip_address))  # <class 'str'>
+
+
+port = 80
+print(port)
+print(type(port))  # <class 'int'>
+
+
+number = 1.5
+print(number)  # 1.5
+print(type(number))  # <class 'float'>
