edb83f73bcf0ac287bd22639460192f82d83c215
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-21 07:48:59 -0500

    module 1 - class 3 _ type casting

---
+++ Module_1/03_Variables_TypeOfData/01_type_casting.py
@@ -0,0 +1,12 @@
+number = 1.5
+print(number)  # 1.5
+print(type(number))  # <class 'float'>
+
+number = int(number)
+print(number)  # 1
+print(type(number))  # <class 'int'>
+
+
+number = str(number)
+print(number)  # 1
+print(type(number))  # <class 'str'>
