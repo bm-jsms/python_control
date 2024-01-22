573bf0223fb8bb700f9c3aafa5ce770f4b5bf992
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-22 17:20:51 -0500

    module 2 - class 4 _ dict comprehension

---
+++ Module_2/04_Dictionaries/01_dict_comprehension.py
@@ -0,0 +1,13 @@
+from modules.divider import divider
+
+
+squares = {x: x**2 for x in range(6)}
+print(squares)
+
+
+divider()
+
+
+print(squares.keys())
+print(squares.values())
+print(squares.items())
