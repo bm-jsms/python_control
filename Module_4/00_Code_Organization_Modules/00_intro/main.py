681cc9e214e05d9841368cc49a2b0a171925e92c
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-28 19:00:55 -0500

    module 4 - class 1 _ intro modules

---
+++ Module_4/00_Code_Organization_Modules/00_intro/main.py
@@ -0,0 +1,10 @@
+from maths import add, sub, mul, div
+
+print(add(3, 2))  # 5
+
+print(sub(3, 2))  # 1
+
+print(mul(7, 12))  # 84
+
+print(div(10, 2))  # 5.0
+print(div(10, 0))  # ValueError: [!] Cannot divide by zero!
