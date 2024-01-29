4f72ebe39cd75b7db41310e29eaebd6ddab28f2f
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-29 17:33:19 -0500

    module 4 - class 2 _ import modules

---
+++ Module_4/01_Import/00_import_modules.py
@@ -0,0 +1,16 @@
+# from math import sqrt
+# print(int(sqrt(144)))  # 1
+
+
+# from math import sqrt as square_root
+# print(int(square_root(144)))  # 1
+
+
+# from math import sqrt as square_root, pi as PI
+# print(int(square_root(144)))  # 12
+# print(PI)  # 3.14159265358979
+
+
+import math as m
+print(int(m.sqrt(144)))  # 12
+print(m.pi)  # 3.141592653589793
