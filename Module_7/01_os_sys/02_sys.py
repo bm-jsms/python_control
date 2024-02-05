825a76581d7865b3e461d274b1191eb1890f6f59
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-05 18:32:38 -0500

    Module 7 - class 1 _ sys

---
+++ Module_7/01_os_sys/02_sys.py
@@ -0,0 +1,6 @@
+import sys
+
+print(f"\n[*] The current version of Python is: {sys.version}\n")
+print(f"\n[*] Name of the script: {sys.argv[0]}\n")
+print(f"\n[*] Number of arguments: {len(sys.argv)}\n")
+print(f"\n[*] Arguments: {sys.argv}\n")
